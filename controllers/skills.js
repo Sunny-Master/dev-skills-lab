99d29183f8f2e5f35135c2ebe19606152b379e6d
Sunny Master <master.codeworks@gmail.com>
2024-08-01 18:47:32 -0400

    complete part 4 of the lab

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,7 +3,7 @@ import { Skill} from '../models/skill.js'
 
 //import { skills } from "../data/skill-data.js"
 
-async function index(req, res){
+async function index(req, res) {
   try {
     const skills = await Skill.find({})
     res.render('skills/index', {
@@ -15,11 +15,11 @@ async function index(req, res){
   }
 }
 
-async function newSkill(req, res){
+async function newSkill(req, res) {
   res.render('skills/new')
 }
 
-async function create(req, res){
+async function create(req, res) {
   try {
     req.body.learned = false
     await Skill.create(req.body)
@@ -30,9 +30,37 @@ async function create(req, res){
   }
 }
 
+async function show(req, res) {
+  try {
+    // find the skill's details
+    const skill = await Skill.findById(req.params.skillId)
+    res.render('skills/show', {
+      skill
+    })
+  } catch (error) {
+    console.log(error)
+    res.redirect('/skills')
+  }
+}
+
+async function deleteSkill(req, res) {
+  try {
+    // find the skill to delete and delete it
+    await Skill.findByIdAndDelete(req.params.skillId)
+    // redirect to /skills
+    res.redirect('/skills')
+  } catch (error) {
+    console.log(error)
+    res.redirect('/skills')
+  }
+}
+
+
 
 export {
   index,
   newSkill as new,
   create,
+  show,
+  deleteSkill as delete,
 }
--- routes/skills.js
+++ routes/skills.js
@@ -10,7 +10,13 @@ router.get('/', skillsCtrl.index)
 // GET localhost:3000/skills/new
 router.get('/new', skillsCtrl.new)
 
+// GET localhost:3000/skills/:skillId
+router.get('/:skillId', skillsCtrl.show)
+
 // POST localhost:3000/skills
 router.post('/', skillsCtrl.create)
+
+// DELETE localhost:3000/skills/:skillId
+router.delete('/:skillId', skillsCtrl.delete)
 
 export { router }
